62fa26517c088f762bfdc8bfc4b933187d3e3012
Danil Lytvyn <danil.googlle@gmail.com>
2023-11-04 23:10:36 +0200

    wip

--- front-end/src/views/tinder/TinderView.vue
+++ front-end/src/views/tinder/TinderView.vue
@@ -1,10 +1,20 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import UsersApi from "../../api/users.api";
+
+const users = ref([]);
+
+onMounted(() => {
+  users.value = UsersApi.getAll();
+});
 </script>
 
 <template>
-Tinder page
+  <v-container class="bg-surface-variant mb-6" style="height: 100vh">
+    <v-row align="center" justify="center" class="h-100">
+      <v-col align-self="center"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
