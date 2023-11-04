1fb17aa264efb7e96fb95d2be9ad38246700feb3
Danil Lytvyn <danil.googlle@gmail.com>
2023-11-04 21:44:04 +0200

    add http service

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/home/HomeView.vue";
+import TinderView from "../views/tinder/TinderView.vue";
 
 const componentRoutes = [
   {
     path: "/",
     component: HomeView,
     name: "home",
+  },
+  {
+    path: "/mate-match",
+    component: TinderView,
+    name: "match",
   },
 ];
 
