b0b4fb2e144f8332e286df54b4d5f21a262a2e3a
Danil Lytvyn <danil.googlle@gmail.com>
2023-11-04 21:56:50 +0200

    Add vuetify plugin to vite

---
+++ front-end/src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+const vuetify = createVuetify({
+  // ... your configuration
+});
+
+export default vuetify;
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-import { resolve, dirname } from "node:path";
+import { resolve } from "node:path";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
