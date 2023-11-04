03fce958ab6072697a574f105a01f8a964fbc5e4
Konstantion <yagudinkostya@gmail.com>
2023-11-04 22:48:48 +0200

    Add exception handler

---
+++ back-end/src/main/java/com/hackaton/makemate/web/controller/exception/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.hackaton.makemate.web.controller.exception;
+
+import com.hackaton.makemate.domain.exception.BadRequestException;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+  // I shit code and i don't care
+  private static final String DEFAULT_KEY = "message";
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<Map<String, String>> handleBadRequestException(BadRequestException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(DEFAULT_KEY, e.getMessage()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Map<String, String>> handleException(Exception e) {
+    return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
+        .body(Map.of(DEFAULT_KEY, e.getMessage()));
+  }
+}
