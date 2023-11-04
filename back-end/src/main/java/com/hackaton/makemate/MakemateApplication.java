8009e0ed2884892b05e4a4a0637a342519353e6c
Konstantion <yagudinkostya@gmail.com>
2023-11-04 17:25:00 +0200

    add back-end directory

---
+++ back-end/src/main/java/com/hackaton/makemate/MakemateApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.makemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemateApplication.class, args);
+	}
+
+}
