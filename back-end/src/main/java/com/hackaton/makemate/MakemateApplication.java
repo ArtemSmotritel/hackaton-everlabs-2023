8d7e23390a94cf49f0b85804fee1aa6dd8b94426
Konstantion <yagudinkostya@gmail.com>
2023-11-05 04:43:01 +0200

    Add mocked data

--- back-end/src/main/java/com/hackaton/makemate/MakemateApplication.java
+++ back-end/src/main/java/com/hackaton/makemate/MakemateApplication.java
@@ -10,146 +10,159 @@ import com.hackaton.makemate.domain.event.Event;
 import com.hackaton.makemate.domain.interest.Interest;
 import com.hackaton.makemate.domain.user.User;
 import jakarta.transaction.Transactional;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class MakemateApplication {
 
-  @Autowired private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @Autowired private InterestRepository interestRepository;
-  @Autowired private EventRepository eventRepository;
+    @Autowired
+    private InterestRepository interestRepository;
+    @Autowired
+    private EventRepository eventRepository;
 
-  public static void main(String[] args) {
-    SpringApplication.run(MakemateApplication.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner commandLineRunner() {
-    return (__) -> {
-      // TODO: create real migration with flyway if possible
-      performInterestMigration();
-      performUserMigration();
-      userRepository.flush();
-      performEventMigration();
-    };
-  }
-
-  @Transactional
-  public void performEventMigration() {
-    Faker faker = new Faker();
-
-    List<Event> interests = eventRepository.findAll();
-    Collections.shuffle(interests);
-
-    long thirtyMinutesInMillis = TimeUnit.MINUTES.toMillis(30);
-
-    long past = System.currentTimeMillis() - thirtyMinutesInMillis;
-    long future = System.currentTimeMillis() + thirtyMinutesInMillis;
-    for (int i = 0; i < 20; i++) {
-      Event event =
-          new Event(
-              null,
-              faker.name().firstName(),
-              faker.name().lastName(),
-              randomDateTimeBetween(past, future),
-              null,
-              EvenType.PUBLIC);
-
-      eventRepository.save(event);
+    public static void main(String[] args) {
+        SpringApplication.run(MakemateApplication.class, args);
     }
-  }
 
-  @Transactional
-  public void performUserMigration() {
-    Faker faker = new Faker();
-    final int userCount = 80;
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return (__) -> {
+            // TODO: create real migration with flyway if possible
+            performInterestMigration();
+            performUserMigration();
+            performEventMigration();
+        };
+    }
 
-    List<Interest> interests = interestRepository.findAll();
+    @Transactional
+    public void performEventMigration() {
+        List<User> users = userRepository.findAll();
+        List<Interest> interests = interestRepository.findAll();
 
-    for (int i = 0; i < 80; i++) {
-      final String description =
-          String.format(
-              "In the age-old lands of Middle-earth, %s is found wandering the paths of %s. "
-                  + "Here, the tales of old come to life and legends walk amongst the living.",
-              faker.lordOfTheRings().character(), faker.lordOfTheRings().location());
+        Faker faker = new Faker();
 
-      User user =
-          new User(
-              null,
-              faker.name().firstName(),
-              faker.name().lastName(),
-              faker.date().birthday(),
-              description,
-              null);
-      user.setInterests(randomSubArray(interests));
-
-      userRepository.save(user);
-      // Don't ask what is it IDK FO
-      if (((userCount | 31) & i) == 0 && (i & (i - 1)) == 0) {
         Collections.shuffle(interests);
-      }
-    }
-  }
 
-  @Transactional
-  public void performInterestMigration() {
-    // Yes I ENJOY SHIT CODING
-    interestRepository.save(new Interest(null, "Sport"));
-    interestRepository.save(new Interest(null, "Music"));
-    interestRepository.save(new Interest(null, "Movie"));
-    interestRepository.save(new Interest(null, "Dancing"));
-    interestRepository.save(new Interest(null, "Cooking"));
-    interestRepository.save(new Interest(null, "Reading"));
-    interestRepository.save(new Interest(null, "Swimming"));
-    interestRepository.save(new Interest(null, "Gaming"));
-    interestRepository.save(new Interest(null, "Traveling"));
-    interestRepository.save(new Interest(null, "Hiking"));
-    interestRepository.save(new Interest(null, "Photography"));
-    interestRepository.save(new Interest(null, "Painting"));
-    interestRepository.save(new Interest(null, "Writing"));
-    interestRepository.save(new Interest(null, "Gardening"));
-    interestRepository.save(new Interest(null, "Knitting"));
-    interestRepository.save(new Interest(null, "Cycling"));
-    interestRepository.save(new Interest(null, "Yoga"));
-    interestRepository.save(new Interest(null, "Meditation"));
-    interestRepository.save(new Interest(null, "Fishing"));
-    interestRepository.save(new Interest(null, "DIY"));
-    interestRepository.save(new Interest(null, "Surfing"));
-    interestRepository.save(new Interest(null, "Skateboarding"));
-    interestRepository.save(new Interest(null, "Blogging"));
-    interestRepository.save(new Interest(null, "Volunteering"));
-    interestRepository.save(new Interest(null, "Coding"));
-    interestRepository.save(new Interest(null, "Board Games"));
-    interestRepository.save(new Interest(null, "Astronomy"));
-    interestRepository.save(new Interest(null, "Magic and Illusion"));
-    interestRepository.save(new Interest(null, "Stand-up Comedy"));
-    interestRepository.save(new Interest(null, "Podcasting"));
-    interestRepository.save(new Interest(null, "Language Learning"));
-    interestRepository.save(new Interest(null, "Home Brewing"));
-    interestRepository.save(new Interest(null, "Aquarium Keeping"));
-  }
+        long thirtyMinutesInMillis = TimeUnit.MINUTES.toMillis(30);
 
-  private List<Interest> randomSubArray(List<Interest> interests) {
-    if (interests.isEmpty()) {
-      return Lists.newArrayList();
+        long past = System.currentTimeMillis() - thirtyMinutesInMillis;
+        long future = System.currentTimeMillis() + thirtyMinutesInMillis;
+        for (int i = 0; i < 20; i++) {
+            Event event =
+                    new Event(
+                            null,
+                            faker.witcher().quote(),
+                            faker.lorem().sentence(),
+                            randomDateTimeBetween(past, future),
+                            faker.witcher().location(),
+                            users.get(i % users.size()),
+                            i % 5 == 0 ? EvenType.PRIVATE : EvenType.PUBLIC);
+
+            event.getParticipants().add(users.get(users.size() - i - 1));
+            event.getParticipants().add(users.get(users.size() - i - 2));
+
+            eventRepository.save(event);
+        }
+
     }
 
-    int size = 1 + new Random().nextInt(Math.min(8, interests.size()));
-    return new ArrayList<>(interests.subList(0, size));
-  }
+    @Transactional
+    public void performUserMigration() {
+        Faker faker = new Faker();
+        final int userCount = 80;
 
-  public static LocalDateTime randomDateTimeBetween(long startMillis, long endMillis) {
-    long randomMillis = startMillis + new Random().nextLong(endMillis);
-    return LocalDateTime.ofInstant(Instant.ofEpochMilli(randomMillis), ZoneId.systemDefault());
-  }
+        List<Interest> interests = interestRepository.findAll();
+
+        for (int i = 0; i < 80; i++) {
+            final String description =
+                    String.format(
+                            "In the age-old lands of Middle-earth, %s is found wandering the paths of %s. "
+                                    + "Here, the tales of old come to life and legends walk amongst the living.",
+                            faker.lordOfTheRings().character(), faker.lordOfTheRings().location());
+
+            User user =
+                    new User(
+                            null,
+                            faker.name().firstName(),
+                            faker.name().lastName(),
+                            faker.date().birthday(),
+                            description,
+                            null);
+            user.setInterests(randomSubArray(interests));
+
+            userRepository.save(user);
+            // Don't ask what is it IDK FO
+            if (((userCount | 31) & i) == 0 && (i & (i - 1)) == 0) {
+                Collections.shuffle(interests);
+            }
+        }
+    }
+
+    @Transactional
+    public void performInterestMigration() {
+        // Yes I ENJOY SHIT CODING
+        interestRepository.save(new Interest(null, "Sport"));
+        interestRepository.save(new Interest(null, "Music"));
+        interestRepository.save(new Interest(null, "Movie"));
+        interestRepository.save(new Interest(null, "Dancing"));
+        interestRepository.save(new Interest(null, "Cooking"));
+        interestRepository.save(new Interest(null, "Reading"));
+        interestRepository.save(new Interest(null, "Swimming"));
+        interestRepository.save(new Interest(null, "Gaming"));
+        interestRepository.save(new Interest(null, "Traveling"));
+        interestRepository.save(new Interest(null, "Hiking"));
+        interestRepository.save(new Interest(null, "Photography"));
+        interestRepository.save(new Interest(null, "Painting"));
+        interestRepository.save(new Interest(null, "Writing"));
+        interestRepository.save(new Interest(null, "Gardening"));
+        interestRepository.save(new Interest(null, "Knitting"));
+        interestRepository.save(new Interest(null, "Cycling"));
+        interestRepository.save(new Interest(null, "Yoga"));
+        interestRepository.save(new Interest(null, "Meditation"));
+        interestRepository.save(new Interest(null, "Fishing"));
+        interestRepository.save(new Interest(null, "DIY"));
+        interestRepository.save(new Interest(null, "Surfing"));
+        interestRepository.save(new Interest(null, "Skateboarding"));
+        interestRepository.save(new Interest(null, "Blogging"));
+        interestRepository.save(new Interest(null, "Volunteering"));
+        interestRepository.save(new Interest(null, "Coding"));
+        interestRepository.save(new Interest(null, "Board Games"));
+        interestRepository.save(new Interest(null, "Astronomy"));
+        interestRepository.save(new Interest(null, "Magic and Illusion"));
+        interestRepository.save(new Interest(null, "Stand-up Comedy"));
+        interestRepository.save(new Interest(null, "Podcasting"));
+        interestRepository.save(new Interest(null, "Language Learning"));
+        interestRepository.save(new Interest(null, "Home Brewing"));
+        interestRepository.save(new Interest(null, "Aquarium Keeping"));
+    }
+
+    private List<Interest> randomSubArray(List<Interest> interests) {
+        if (interests.isEmpty()) {
+            return Lists.newArrayList();
+        }
+
+        int size = 1 + new Random().nextInt(Math.min(8, interests.size()));
+        return new ArrayList<>(interests.subList(0, size));
+    }
+
+    public static LocalDateTime randomDateTimeBetween(long startMillis, long endMillis) {
+        long randomMillis = startMillis + new Random().nextLong(endMillis);
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(randomMillis), ZoneId.systemDefault());
+    }
 }
--- back-end/src/main/java/com/hackaton/makemate/domain/event/Event.java
+++ back-end/src/main/java/com/hackaton/makemate/domain/event/Event.java
@@ -4,6 +4,7 @@ import com.google.common.base.Objects;
 import com.hackaton.makemate.domain.interest.Interest;
 import com.hackaton.makemate.domain.user.User;
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
@@ -12,145 +13,151 @@ import java.util.Set;
 @Entity
 @Table(name = "event")
 public class Event {
-  @Id
-  @Column(name = "id", updatable = false)
-  @GeneratedValue
-  private Long id;
+    @Id
+    @Column(name = "id", updatable = false)
+    @GeneratedValue
+    private Long id;
 
-  @Enumerated(EnumType.STRING)
-  private EvenType type;
+    @Enumerated(EnumType.STRING)
+    private EvenType type;
 
-  private String name;
-  private String description;
-  private LocalDateTime dateOfEvent;
+    private String name;
+    private String description;
+    private LocalDateTime dateOfEvent;
 
-  @ManyToOne private User createdBy;
+    @ManyToOne
+    private User createdBy;
 
-  @ManyToMany private Set<User> participants = new HashSet<>();
+    @ManyToMany
+    private Set<User> participants = new HashSet<>();
 
-  @ManyToMany private Set<Interest> interests = new HashSet<>();
+    @ManyToMany
+    private Set<Interest> interests = new HashSet<>();
 
-  private String place;
+    private String place;
 
-  public Event(
-      Long id,
-      String name,
-      String description,
-      LocalDateTime dateOfEvent,
-      User createdBy,
-      EvenType type) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.dateOfEvent = dateOfEvent;
-    this.createdBy = createdBy;
-    this.type = type;
-  }
+    public Event(
+            Long id,
+            String name,
+            String description,
+            LocalDateTime dateOfEvent,
+            String place,
+            User createdBy,
+            EvenType type) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.dateOfEvent = dateOfEvent;
+        this.createdBy = createdBy;
+        this.place = place;
+        this.type = type;
+    }
 
-  public Event() {}
+    public Event() {
+    }
 
-  public Set<User> getAllUsers() {
-    Set<User> part = new HashSet<>(participants);
-    part.add(createdBy);
-    return part;
-  }
+    public Set<User> getAllUsers() {
+        Set<User> part = new HashSet<>(participants);
+        part.add(createdBy);
+        return part;
+    }
 
-  public String getDateString() {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM h a");
+    public String getDateString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM h a");
 
-    return dateOfEvent.format(formatter);
-  }
+        return dateOfEvent.format(formatter);
+    }
 
-  public EvenType getType() {
-    return type;
-  }
+    public EvenType getType() {
+        return type;
+    }
 
-  public void setType(EvenType eventType) {
-    this.type = eventType;
-  }
+    public void setType(EvenType eventType) {
+        this.type = eventType;
+    }
 
-  public Set<Interest> getInterests() {
-    return interests;
-  }
+    public Set<Interest> getInterests() {
+        return interests;
+    }
 
-  public void setInterests(Set<Interest> interests) {
-    this.interests = interests;
-  }
+    public void setInterests(Set<Interest> interests) {
+        this.interests = interests;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public LocalDateTime getDateOfEvent() {
-    return dateOfEvent;
-  }
+    public LocalDateTime getDateOfEvent() {
+        return dateOfEvent;
+    }
 
-  public void setDateOfEvent(LocalDateTime dateOfEvent) {
-    this.dateOfEvent = dateOfEvent;
-  }
+    public void setDateOfEvent(LocalDateTime dateOfEvent) {
+        this.dateOfEvent = dateOfEvent;
+    }
 
-  public User getCreatedBy() {
-    return createdBy;
-  }
+    public User getCreatedBy() {
+        return createdBy;
+    }
 
-  public void setCreatedBy(User createdBy) {
-    this.createdBy = createdBy;
-  }
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
 
-  public String getPlace() {
-    return place;
-  }
+    public String getPlace() {
+        return place;
+    }
 
-  public void setPlace(String place) {
-    this.place = place;
-  }
+    public void setPlace(String place) {
+        this.place = place;
+    }
 
-  public Set<User> getParticipants() {
-    return participants;
-  }
+    public Set<User> getParticipants() {
+        return participants;
+    }
 
-  public void setParticipants(Set<User> participants) {
-    this.participants = participants;
-  }
+    public void setParticipants(Set<User> participants) {
+        this.participants = participants;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Event event = (Event) o;
-    return Objects.equal(id, event.id)
-        && type == event.type
-        && Objects.equal(name, event.name)
-        && Objects.equal(description, event.description)
-        && Objects.equal(dateOfEvent, event.dateOfEvent)
-        && Objects.equal(createdBy, event.createdBy)
-        && Objects.equal(participants, event.participants)
-        && Objects.equal(place, event.place);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equal(id, event.id)
+                && type == event.type
+                && Objects.equal(name, event.name)
+                && Objects.equal(description, event.description)
+                && Objects.equal(dateOfEvent, event.dateOfEvent)
+                && Objects.equal(createdBy, event.createdBy)
+                && Objects.equal(participants, event.participants)
+                && Objects.equal(place, event.place);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(
-        id, type, name, description, dateOfEvent, createdBy, participants, place);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(
+                id, type, name, description, dateOfEvent, createdBy, participants, place);
+    }
 }
