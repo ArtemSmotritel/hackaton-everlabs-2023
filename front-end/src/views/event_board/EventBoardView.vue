91fe0b05dc1f15b05114d3d808390fe0921fca0a
Artem Smotrytel <artemsmotritel@gmail.com>
2023-11-05 14:32:48 +0200

    add 'nothing here' message in event board

--- front-end/src/views/event_board/EventBoardView.vue
+++ front-end/src/views/event_board/EventBoardView.vue
@@ -33,74 +33,84 @@ onMounted(async () => {
     <v-window v-model="tab" class="mt-12">
       <v-window-item value="general">
         <v-row>
-          <v-col
-            v-for="event in generalEvents"
-            :key="event.id"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <EventCard :board-event="event" interestTitle="Looking for">
-              <template
-                v-if="event.interests?.length > 0"
-                v-slot:interests-title
-              >
-                <strong>Looking for:</strong>
-              </template>
-              <template v-slot:interests>
-                <div
-                  v-if="event.interests?.length > 0"
-                  style="height: 80px; overflow-y: auto"
-                >
-                  <AppInterestChip
-                    v-for="(interest, index) in event.interests"
-                    :key="index"
-                    :text="interest.name"
-                    style="
-                      margin-bottom: 0 !important;
-                      margin-right: 0 !important;
-                    "
-                  />
-                </div>
-              </template>
-            </EventCard>
+          <v-col v-if="generalEvents.length <= 0" cols="12" md="6" lg="4">
+            <p>Nothing here &#129402; &#x1F97A;</p>
           </v-col>
+          <template v-else>
+            <v-col
+              v-for="event in generalEvents"
+              :key="event.id"
+              cols="12"
+              md="6"
+              lg="4"
+            >
+              <EventCard :board-event="event" interestTitle="Looking for">
+                <template
+                  v-if="event.interests?.length > 0"
+                  v-slot:interests-title
+                >
+                  <strong>Looking for:</strong>
+                </template>
+                <template v-slot:interests>
+                  <div
+                    v-if="event.interests?.length > 0"
+                    style="height: 80px; overflow-y: auto"
+                  >
+                    <AppInterestChip
+                      v-for="(interest, index) in event.interests"
+                      :key="index"
+                      :text="interest.name"
+                      style="
+                        margin-bottom: 0 !important;
+                        margin-right: 0 !important;
+                      "
+                    />
+                  </div>
+                </template>
+              </EventCard>
+            </v-col>
+          </template>
         </v-row>
       </v-window-item>
       <v-window-item value="private">
         <v-row>
-          <v-col
-            v-for="event in privateEvents"
-            :key="event.id"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <EventCard :board-event="event" interestTitle="Looking for">
-              <template
-                v-if="event.interests?.length > 0"
-                v-slot:interests-title
-              >
-                <strong>Looking for:</strong>
-              </template>
-              <template v-slot:interests>
-                <div
-                  v-if="event.interests?.length > 0"
-                  style="height: 80px; overflow-y: auto"
-                >
-                  <AppInterestChip
-                    v-for="(interest, index) in event.interests"
-                    :key="index"
-                    :text="interest.name"
-                    style="
-                      margin-bottom: 0 !important;
-                      margin-right: 0 !important;
-                    "
-                  />
-                </div>
-              </template>
-            </EventCard>
+          <v-col v-if="privateEvents.length <= 0" cols="12" md="6" lg="4">
+            <p>Nothing here &#129402; &#x1F97A;</p>
           </v-col>
+          <template v-else>
+            <v-col
+              v-for="event in privateEvents"
+              :key="event.id"
+              cols="12"
+              md="6"
+              lg="4"
+            >
+              <EventCard :board-event="event" interestTitle="Looking for">
+                <template
+                  v-if="event.interests?.length > 0"
+                  v-slot:interests-title
+                >
+                  <strong>Looking for:</strong>
+                </template>
+                <template v-slot:interests>
+                  <div
+                    v-if="event.interests?.length > 0"
+                    style="height: 80px; overflow-y: auto"
+                  >
+                    <AppInterestChip
+                      v-for="(interest, index) in event.interests"
+                      :key="index"
+                      :text="interest.name"
+                      style="
+                        margin-bottom: 0 !important;
+                        margin-right: 0 !important;
+                      "
+                    />
+                  </div>
+                </template>
+              </EventCard>
+            </v-col>
+          </template>
         </v-row>
       </v-window-item>
     </v-window>
